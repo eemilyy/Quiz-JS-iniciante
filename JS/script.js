ba2a6e43b9f03cf96823e81257833b66eb37d2c2
Emily Santos <emily.mimy12@gmail.com>
2021-03-06 18:14:55 -0300

    pegando nome de input

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,15 @@ var idade = window.prompt("Sua idade:");
 window.alert(`A ${nome} tem ${idade} anos`);
 console.log(nome.length);*/
 
+var clicou = window.document.getElementById('botao');
+//var clicou = getElementById('botao');
+clicou.addEventListener('click', nomear);
+
+
+function nomear(){
+    var caixaNome = window.document.getElementById('nomeJogador');
+    var nome = String(caixaNome.value);
+    console.log(nome);
+    var para = window.document.querySelector('#texto');
+    para.innerHTML = `Bem vinda ${nome}`;
+}
