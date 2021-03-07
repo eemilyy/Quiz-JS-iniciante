3ca6cce9193df414efa0e3592ef5e9f1ef5fddc8
Emily Santos <emily.mimy12@gmail.com>
2021-03-06 22:48:27 -0300

    validando entrada

--- JS/script.js
+++ JS/script.js
@@ -3,14 +3,19 @@ var idade = window.prompt("Sua idade:");
 window.alert(`A ${nome} tem ${idade} anos`);
 console.log(nome.length);*/
 
-var clicou = window.document.getElementById('botao');
+var clicou = window.document.getElementById('submit');
 clicou.addEventListener('click', nomear);
 
 
 function nomear(){
-    var caixaNome = window.document.getElementById('nomeJogador').value;
+    var nome = window.document.getElementById('nomeJogador').value;
     //var nome = String(caixaNome.value);
-    console.log(caixaNome);
+    //console.log(Number(nome));
+
+    if(!isNaN(nome)){  //verificar pois aceita numeros com letras
+        window.alert("Digite apenas letras!");
+    }
+
     var para = window.document.querySelector('#texto');
-    para.innerHTML = `Bem vinda ${caixaNome}`;
+    para.innerHTML = `Bem vinda ${nome}`;
 }
