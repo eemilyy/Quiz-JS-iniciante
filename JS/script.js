7d994bad9e65e6d1547059b21860104075ac70ea
Emily Santos <emily.mimy12@gmail.com>
2021-03-06 23:09:20 -0300

    colocando bordas CSS

--- JS/script.js
+++ JS/script.js
@@ -3,12 +3,12 @@ var idade = window.prompt("Sua idade:");
 window.alert(`A ${nome} tem ${idade} anos`);
 console.log(nome.length);*/
 
-var clicou = window.document.getElementById('submit');
-clicou.addEventListener('click', nomear);
+var clicou = window.document.getElementById('submit'); //pegando o botão do HTML
+clicou.addEventListener('click', nomear); //caso o usuário clique, ele chama a função nomear()
 
 
 function nomear(){
-    var nome = window.document.getElementById('nomeJogador').value;
+    var nome = window.document.getElementById('nomeJogador').value; //pegando o valor digitado na caixa de texto
     //var nome = String(caixaNome.value);
     //console.log(Number(nome));
 
@@ -16,6 +16,6 @@ function nomear(){
         window.alert("Digite apenas letras!");
     }
 
-    var para = window.document.querySelector('#texto');
-    para.innerHTML = `Bem vinda ${nome}`;
+    var para = window.document.querySelector('#texto'); //criando variável para pegar o p que está no HTML
+    para.innerHTML = `Bem vinda ${nome}`; //innerHTML aparece texto
 }
