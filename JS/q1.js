d433058dc2b7848bfc3e14bf03d460248ccf0933
Emily Santos <emily.mimy12@gmail.com>
2021-03-08 20:58:14 -0300

    alterando questão1

--- JS/q1.js
+++ JS/q1.js
@@ -1,14 +1,33 @@
 /*function questao1(valor){
     if(valor !== 'Brasília'){
-        console.log("Errado!");
+        var resposta = window.document.querySelectorAll("input").;
+        document..style.background = "green";
     }
 }*/
 
-var clicou = document.querySelector("#q1");
+/*var clicou = document.querySelector("#q1");
 
 function q(){
     var r = window.document.querySelector(".botao").value;
     console.log(r);
 }
 
-clicou.addEventListener("click", q);
+clicou.addEventListener("click", q);*/
+
+var botoes = document.querySelectorAll('input[type="button"]');
+for (var i = 0; i < botoes.length; i++){
+  botoes[i].addEventListener('click', function(){
+       // e aqui, o this é o botão clicado
+       var resposta = this.id;
+       if(resposta !== 'r3'){
+           window.document.getElementById(resposta).style.background = "red";
+       }
+       else{
+            window.document.getElementById(resposta).style.background = "green";
+            
+       }
+       //var classes = this.classList;
+  });
+
+  
+}
