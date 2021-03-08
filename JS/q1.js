28f8144d8a4c79a2143dd303c2998b744dc5b3f3
Emily Santos <emily.mimy12@gmail.com>
2021-03-07 22:09:08 -0300

    inserindo pag 2

--- JS/q1.js
+++ JS/q1.js
@@ -1,5 +1,14 @@
-function questao1(valor){
+/*function questao1(valor){
     if(valor !== 'Brasília'){
         console.log("Errado!");
     }
+}*/
+
+var clicou = document.querySelector("#q1");
+
+function q(){
+    var r = window.document.querySelector(".botao").value;
+    console.log(r);
 }
+
+clicou.addEventListener("click", q);
