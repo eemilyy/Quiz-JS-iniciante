b1bb2176a2e26f992713033c1cc1cdab27335a37
Emily Santos <emily.mimy12@gmail.com>
2021-03-10 18:46:20 -0300

    Inserindo q3

--- JS/q3.js
+++ JS/q3.js
@@ -1,42 +1,23 @@
-/*function questao1(valor){
-    if(valor !== 'Brasília'){
-        var resposta = window.document.querySelectorAll("input").;
-        document..style.background = "green";
-    }
-}*/
-
-/*var clicou = document.querySelector("#q1");
-
-function q(){
-    var r = window.document.querySelector(".botao").value;
-    console.log(r);
-}
-
-clicou.addEventListener("click", q);*/
-
 var botoes = document.querySelectorAll('input[type="button"]');
 for (var i = 0; i < botoes.length; i++){
   botoes[i].addEventListener('click', function(){
        // e aqui, o this é o botão clicado
        var resposta = this.id;
-       if(resposta !== 'r3'){
+       if(resposta !== 'r4'){
            window.document.getElementById(resposta).style.background = "red";
 
            botoes.disable = true;
 
-
            setTimeout(function() { //colocando tempo para poder executar a chamada da proxima pag
-            window.location.href = "q2.html";
+            window.location.href = "q4.html";
         }, 1000);
        }
        else{
             window.document.getElementById(resposta).style.background = "green";
 
             setTimeout(function() { //colocando tempo para poder executar a chamada da proxima pag
-                window.location.href = "q2.html";
+                window.location.href = "q4.html";
             }, 1000);
        }
   });
-
-
 }
