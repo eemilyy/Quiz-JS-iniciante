0870ef16bfd908bc7fda0719ac5cd559e2a84b2c
Emily Santos <emily.mimy12@gmail.com>
2021-03-10 19:33:13 -0300

    inserindo buscas

---
+++ JS/q4.js
@@ -0,0 +1,23 @@
+var botoes = document.querySelectorAll('input[type="button"]');
+for (var i = 0; i < botoes.length; i++){
+  botoes[i].addEventListener('click', function(){
+       // e aqui, o this é o botão clicado
+       var resposta = this.id;
+       if(resposta !== 'r3'){
+           window.document.getElementById(resposta).style.background = "red";
+
+           botoes.disable = true;
+
+           setTimeout(function() { //colocando tempo para poder executar a chamada da proxima pag
+            window.location.href = "q5.html";
+        }, 1000);
+       }
+       else{
+            window.document.getElementById(resposta).style.background = "green";
+
+            setTimeout(function() { //colocando tempo para poder executar a chamada da proxima pag
+                window.location.href = "q5.html";
+            }, 1000);
+       }
+  });
+}
